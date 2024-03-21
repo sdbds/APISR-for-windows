68f6e04041e47d438b1ba51428e3215c1f63c7d7
青龍聖者@bdsqlsz <qinglongshengzhe@gmail.com>
2024-03-21 11:46:13 +0800

    Update run_batch.ps1

--- run_batch.ps1
+++ run_batch.ps1
@@ -1,6 +1,9 @@
-$input_dir="./__assets__/lr_inputs"
-$store_dir="./__assets__/visual_results"
-$weight_path="./pretrained/4x_APISR_GRL_GAN_generator.pth"
+$input_dir="F:\\input"
+$store_dir="F:\\output"
+$weight_path="./pretrained/2x_APISR_RRDB_GAN_generator.pth"
+$scale=2
+$model="RRDB"
+$float16_inference=1
 
 Set-Location $PSScriptRoot
 .\venv\Scripts\activate
@@ -9,5 +12,15 @@ $Env:HF_HOME = "./huggingface"
 $Env:XFORMERS_FORCE_DISABLE_TRITON = "1"
 $Env:PYTHONPATH = $PSScriptRoot
 $Env:HF_ENDPOINT = "https://hf-mirror.com"
+$ext_args = [System.Collections.ArrayList]::new()
 
-python.exe "test_code/inference.py" --input_dir=$input_dir --weight_path=$weight_path --store_dir=$store_dir
+if ($float16_inference) {
+  [void]$ext_args.Add("--float16_inference=True")
+}
+
+python.exe "test_code/inference.py" `
+--input_dir=$input_dir `
+--weight_path=$weight_path `
+--store_dir=$store_dir `
+--scale=$scale `
+--model=$model $ext_args
